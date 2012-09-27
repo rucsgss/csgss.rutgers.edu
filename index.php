28ded44fba7f7d0383bb20533b13298620a96227
Jeff Ames <jeff@anjefu.com>
2012-09-27 16:40:34 -0400

    Add images for homepage, remove thesis image

--- index.php
+++ index.php
@@ -11,14 +11,22 @@
         <h1>News from CSGSS</h1>
         <div class="news">
           <div class="newsitem">
-            <h2>Welcome back!</h2>
-            <h3>Sept 4, 2012</h3>
-            <p>Welcome back for Fall 2012!</p>
+            <div class="thumbnail"><img src="/images/fall2012.jpg" height=299 width=400 alt="Rutgers in fall" class="thumbnail"/></div>
+            <div>
+              <h2>Welcome back!</h2>
+              <h3>Sept 4, 2012</h3>
+              <p>Welcome back for Fall 2012!</p>
+            </div>
+            <div class="clear"></div>
           </div>
           <div class="newsitem">
-            <h2>Martin Farach-Colton wins Award for Excellence in Teaching</h2>
-            <h3>May 15, 2012</h3>
-            <p>The Computer Science Graduate Student Society (CSGSS) of the Rutgers CS department selected the winner for this year's top teacher. Monica Babes, CSGSS President, announced last week that the graduate students chose Prof. Martin Farach-Colton for the 2011-2012 CSGSS Award for Excellence in Teaching.</p>
+            <div class="thumbnail"><img src="/images/martin-award.jpg" height=221 width=400 alt="Martin Farach-Colton receiving teaching award" class="thumbnail"/></div>
+            <div>
+              <h2>Martin Farach-Colton wins Award for Excellence in Teaching</h2>
+              <h3>May 15, 2012</h3>
+              <p>The Computer Science Graduate Student Society (CSGSS) of the Rutgers CS department selected the winner for this year's top teacher. Monica Babes, CSGSS President, announced last week that the graduate students chose Prof. Martin Farach-Colton for the 2011-2012 CSGSS Award for Excellence in Teaching.</p>
+            </div>
+            <div class="clear"></div>
           </div>
         </div>
       </div>
--- resources/index.php
+++ resources/index.php
@@ -40,20 +40,17 @@ chmod 644 ~/public_html/index.html</code></pre>
           </div>
           <div><div class="arrow"></div><p class="heading">Write your thesis in LaTeX</p></div>
           <div class="content">
-            <div class="thumbnail"><img src="/images/thesis.png" alt="Thesis title page" class="thumbnail"/></div>
-            <div class="thumbnail-body">
-              <p>The following LaTeX style file is a version of the <a href="http://www.math.rutgers.edu/grad/phd_requirements/thesis.html">style file</a> developed by the Department of Mathematics, customized for students in the Department of Computer Science. It is based on the official university <a href="http://gsnb.rutgers.edu/style_guide.php3">style guide</a>.</p>
-              <ul>
-                <li><a href="thesis.tex">thesis.tex</a> &mdash; a barebones LaTeX file for your thesis.</li>
-                <li><a href="ruthesis.cls">ruthesis.cls</a> &mdash; a LaTeX style file for your thesis.</li>
-              </ul>
-              <p>To use this style file, download the files below, put them in a directory, and build using <code>pdflatex</code>:</p>
-              <div class="code">
-                <pre><code>mkdir ~/thesis
+            <p>The following LaTeX style file is a version of the <a href="http://www.math.rutgers.edu/grad/phd_requirements/thesis.html">style file</a> developed by the Department of Mathematics, customized for students in the Department of Computer Science. It is based on the official university <a href="http://gsnb.rutgers.edu/style_guide.php3">style guide</a>.</p>
+            <ul>
+              <li><a href="thesis.tex">thesis.tex</a> &mdash; a barebones LaTeX file for your thesis.</li>
+              <li><a href="ruthesis.cls">ruthesis.cls</a> &mdash; a LaTeX style file for your thesis.</li>
+            </ul>
+            <p>To use this style file, download the files below, put them in a directory, and build using <code>pdflatex</code>:</p>
+            <div class="code">
+              <pre><code>mkdir ~/thesis
 mv ~/Downloads/thesis.tex ~/Downloads/ruthesis.cls ~/thesis
 cd ~/thesis
 pdflatex thesis</code></pre>
-              </div>
             </div>
           </div>
         </div>
