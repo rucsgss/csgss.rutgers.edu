9402f03452fa0fea9a6c1e196887a5f8df37d641
Jeff Ames <jeff@anjefu.com>
2013-05-11 10:05:01 -0400

    Update officers

--- officers/index.php
+++ officers/index.php
@@ -13,19 +13,15 @@
           <div class="current-officers">
             <ul>
               <li>
-                <p><strong>President:</strong> <a href="mailto:romoore@cs.rutgers.edu">Rob Moore</a><br/><br/></p>
+                <p><strong>President:</strong> <a href="mailto:jeff.ames@cs.rutgers.edu">Jeff Ames</a><br/><br/></p>
                 <p>The president oversees the general operation of CSGSS and is the one to contact about matters that require immediate attention. The president is the main contact between the department chair and the graduate students.</p>
               </li>
               <li>
-                <p><strong>Vice President:</strong> <a href="mailto:hcwoithe@cs.rutgers.edu">Hans Christian Woithe</a><br/><br/></p>
+                <p><strong>Vice President:</strong> <a href="mailto:bcm84@cs.rutgers.edu">Brian McMahan</a><br/><br/></p>
                 <p>The vice president shares the workload of the president and possibly inherits it in the case of presidential absence. The vice president is responsible for keeping a watchful eye over all of the activities of the various committees.</p>
               </li>
               <li>
-                <p><strong>Secretary:</strong> <a href="mailto:wkatsak@cs.rutgers.edu">William Katsak</a><br/><br/></p>
-                <p>The secretary is responsible for keeping the records of and reporting on all non-financial activities of CSGSS.</p>
-              </li>
-              <li>
-                <p><strong>Treasurer:</strong> <a href="mailto:mdhaque@cs.rutgers.edu">Md E. Haque</a><br/><br/></p>
+                <p><strong>Treasurer:</strong> <a href="mailto:amrbakry@cs.rutgers.edu">Amr Bakry</a><br/><br/></p>
                 <p>The treasurer is responsible for managing the financial operations of all CSGSS-sponsored events.</p>
               </li>
             </ul>
@@ -34,31 +30,29 @@
             <h1>Previous Officers</h1>
 
             <div class="year">
-              <h2>2011 &mdash; 2012</h2>
+              <h2>2012 &mdash; 2013</h2>
               <ul>
-                <li>President: Monica Babes-Vroman</li>
-                <li>Vice President: </li>
-                <li>Secretary: </li>
-                <li>Treasurer: </li>
+                <li>President: Rob Moore</li>
+                <li>Vice President: Hans Christian Woithe</li>
+                <li>Secretary: William Katsak</li>
+                <li>Treasurer: Md E. Haque</li>
               </ul>
             </div>
 
             <div class="year">
-              <h2>2010 &mdash; 2011</h2>
+              <h2>2011 &mdash; 2012</h2>
               <ul>
-                <li>President: </li>
-                <li>Vice President: </li>
-                <li>Secretary: </li>
-                <li>Treasurer: </li>
+                <li>President: Monica Babes-Vroman</li>
               </ul>
             </div>
+
+            <!-- 2010 &mdash; 2011 -->
 
             <div class="year">
               <h2>2009 &mdash; 2010</h2>
               <ul>
                 <li>President: William Katsak</li>
                 <li>Vice President: Ana Paula Centeno</li>
-                <li>Secretary: </li>
                 <li>Treasurer: Tom Walsh</li>
               </ul>
             </div>
@@ -93,9 +87,6 @@
               <h2>2004 &mdash; 2005</h2>
               <ul>
                 <li>President: Carlos Diuk</li>
-                <li>Vice President:</li>
-                <li>Secretary:</li>
-                <li>Treasurer:</li>
               </ul>
             </div>
 
