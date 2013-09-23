ec02c802a9e7367ed921ff24ad6cc3901e8ff7ab
Jeff Ames <jeff@anjefu.com>
2013-09-23 14:37:27 -0400

    Add previous officers

--- officers/index.php
+++ officers/index.php
@@ -46,7 +46,11 @@
               </ul>
             </div>
 
-            <!-- 2010 &ndash; 2011 -->
+<!--
+            <div class="year">
+              <h2>2010 &ndash; 2011</h2>
+            </div>
+-->
 
             <div class="year">
               <h2>2009 &ndash; 2010</h2>
@@ -67,19 +71,28 @@
               </ul>
             </div>
 
-            <!-- 2007 &ndash; 2008 -->
-            <!-- 2006 &ndash; 2007 -->
+<!--
+            <div class="year">
+              <h2>2007 &ndash; 2008</h2>
+            </div>
+-->
+
+<!--
+            <div class="year">
+              <h2>2006 &ndash; 2007</h2>
+            </div>
+-->
 
             <div class="year">
               <h2>2005 &ndash; 2006</h2>
               <ul>
                 <li>President: Rohan Fernandes</li>
-                <li>Vice-President: Andre Madeira</li>
+                <li>Vice President: Andre Madeira</li>
                 <li>Secretary: Brian Russell</li>
                 <li>Treasurer: Shu Chen</li>
                 <li>Webmaster: Tim Edmunds</li>
                 <li>CS_CAREERS: Nishkam Ravi</li>
-                <li>LCSR User Group Rep: Pravin Shankar</li>
+                <li>LCSR User Group Representative: Pravin Shankar</li>
               </ul>
             </div>
 
@@ -90,6 +103,203 @@
               </ul>
             </div>
 
+<!--
+            <div class="year">
+              <h2>2003 &ndash; 2004</h2>
+            </div>
+-->
+
+<!--
+            <div class="year">
+              <h2>2002 &ndash; 2003</h2>
+            </div>
+-->
+
+            <div class="year">
+              <h2>2001 &ndash; 2002</h2>
+              <ul>
+                <li>President: Marcelo Mydlarz</li>
+                <li>VP (Academic Chair, Liaison): Andrei Anghelescu</li>
+                <li>VP (Social Chair): Eduardo Pinherio</li>
+                <li>Secretary (Public Relations): Navin Goyal</li>
+                <li>Secretary (Webmaster): Anthony Santella</li>
+                <li>Treasurers: Steve Sanbeg and Kiran Nagaraj</li>
+                <li>Graduate Committee Representative: Aniruddha Bohrai</li>
+                <li>Computing Committee Representative: Ankur Choski</li>
+                <li>Hiring Committee Representative: Dimitri Fradkin</li>
+                <li>Sporting Activities Coordinator: Christopher Peery</li>
+              </ul>
+            </div>
+
+            <div class="year">
+              <h2>2000 &ndash; 2001</h2>
+              <ul>
+                <li>President: Murali Rangarajan</li>
+                <li>Vice President: Taliver Heath and Marcelo Mydlarz</li>
+                <li>Secretary: Christopher Peery and Francisco Cuenca-Acuna</li>
+                <li>Treasurers: Zoltan Jarai and Kiran Nagaraja</li>
+                <li>Computing Representative: Detlef Ronneburger</li>
+                <li>Graduate Committee Representative: John Iacono</li>
+              </ul>
+            </div>
+
+            <div class="year">
+              <h2>1999 &ndash; 2000</h2>
+              <ul>
+                <li>President: Stefan Langerman</li>
+                <li>Vice Presidents: John Iacono and Marcelo Mydlarz</li>
+                <li>Secretaries: Murali Rangarajan and Mark Shoulson</li>
+                <li>Treasurers: Suresh Gopalakrishnan and Aynur Dayanik</li>
+                <li>Graduate Committee Representative: Sachin Lodha</li>
+              </ul>
+            </div>
+
+<!--
+            <div class="year">
+              <h2>1998 &ndash; 1999</h2>
+            </div>
+-->
+
+            <div class="year">
+              <h2>1997 &ndash; 1998</h2>
+              <ul>
+                <li>President: Vincenzo Liberatore</li>
+                <li>Vice Presidents: Ayman Khalfalah and Arshad Shaikh</li>
+                <li>Secretaries: Stefan Langerman and Sachin Lodha</li>
+                <li>Treasurers: Khaled Elbassioni and Makrina Kartezini</li>
+                <li>GAESSCO Representative: Pradeep Sudame</li>
+              </ul>
+            </div>
+
+            <div class="year">
+              <h2>1996 &ndash; 1997</h2>
+              <ul>
+                <li>President: Julio C. Navas</li>
+                <li>Vice Presidents: Naveen Agarwal and Adrian Dumitrescu</li>
+                <li>Secretaries: Sofus Macskassy and Shirish Phatak</li>
+                <li>Treasurers: Vivek Menezes and Glenn Rhoads</li>
+                <li>GAESSCO Representative: Vincenzo Liberatore</li>
+              </ul>
+            </div>
+
+            <div class="year">
+              <h2>1995 &ndash; 1996</h2>
+              <ul>
+                <li>President: Julio C. Navas</li>
+                <li>Vice Presidents: Naveen Agarwal and Peter McIlroy</li>
+                <li>Secretaries: Vincenzo Liberatore and Georgios Katsaros</li>
+                <li>Treasurer: Aaron Adelman</li>
+                <li>Social coordinator: Prasad Deshpande</li>
+                <li>GAESSCO Representative: Aashu Virmani</li>
+              </ul>
+            </div>
+
+<!--
+            <div class="year">
+              <h2>1994 &ndash; 1995</h2>
+            </div>
+-->
+
+            <div class="year">
+              <h2>1993 &ndash; 1994</h2>
+              <ul>
+                <li>President: S Viswanathan</li>
+                <li>Vice Presidents: Brian D Davidson and Julio Cesar Navas</li>
+                <li>Secretaries: Monish Gupta and Pradeep Sudame</li>
+                <li>Treasurers: Vincenzo Liberatore and Chris Mesterharm</li>
+                <li>Bagel God: Maziar Mohammadi</li>
+              </ul>
+            </div>
+
+            <div class="year">
+              <h2>1992 &ndash; 1993</h2>
+              <ul>
+                <li>President: Nathalie Japkowicz</li>
+                <li>Vice Presidents: Ajay Bakre and Aashu Virmani</li>
+                <li>Treasurers: Mark Schwabacher and Saibal Patra</li>
+                <li>Secretaries: Vinayak Karnataki and Arunava Banerjee</li>
+              </ul>
+            </div>
+
+            <div class="year">
+              <h2>1991 &ndash; 1992</h2>
+              <ul>
+                <li>President: Joe Peters</li>
+                <li>Vice Presidents: Souri Das and John Keane</li>
+                <li>Secretaries: Ajay Bakre and Ke-Thia Yao </li>
+                <li>Treasurers: Mark Schwabacher and Christos Kirgios</li>
+              </ul>
+            </div>
+
+            <div class="year">
+              <h2>1990 &ndash; 1991</h2>
+              <ul>
+                <li>President: Phil Bohannon</li>
+                <li>Vice Presidents: Souri Das and Joe Peters</li>
+                <li>Secretaries: Greg Hagerty and Ron Spinner</li>
+                <li>Treasurers: Scott Burgess and Shantanu Shee</li>
+              </ul>
+            </div>
+
+<!--
+            <div class="year">
+              <h2>1989 &ndash; 1990</h2>
+            </div>
+-->
+
+<!--
+            <div class="year">
+              <h2>1988 &ndash; 1989</h2>
+            </div>
+-->
+
+<!--
+            <div class="year">
+              <h2>1987 &ndash; 1988</h2>
+            </div>
+-->
+
+            <div class="year">
+              <h2>1986 &ndash; 1987</h2>
+              <ul>
+                <li>President: Mike Berman</li>
+                <li>Vice President: John Smith</li>
+                <li>Secretaries: Jody Gevins and Chun Liew</li>
+                <li>Treasurers: Wes Braudaway and Liping Sun</li>
+              </ul>
+            </div>
+
+            <div class="year">
+              <h2>1985 &ndash; 1986</h2>
+              <ul>
+                <li>President: Fritz Henglein</li>
+                <li>Vice President: Rich Kempinski</li>
+                <li>Secretaries: Smadar Kedar-Cabelli and Jody Gevins</li>
+                <li>Treasurers: John Smith and Patricia Friedmann</li>
+              </ul>
+            </div>
+
+<!--
+            <div class="year">
+              <h2>1984 &ndash; 1985</h2>
+            </div>
+-->
+
+<!--
+            <div class="year">
+              <h2>1983 &ndash; 1984</h2>
+            </div>
+-->
+
+            <div class="year">
+              <h2>1982 &ndash; 1983</h2>
+              <ul>
+                <li>President: E. C. Pariser</li>
+                <li>Vice President: P. R. Spool</li>
+                <li>Treasurer: R. M. Keller</li>
+              </ul>
+            </div>
+
           </div>
         </div>
       </div>
@@ -97,6 +307,3 @@
     </div>
   </body>
 </html>
-
-
-
