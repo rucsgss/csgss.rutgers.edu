f6bedbf6e03c36fbc76c6675551d57c418e0e5ec
Jeff Ames <jeff@anjefu.com>
2013-10-03 12:59:23 -0400

    Add department info page

---
+++ department-info/index.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html>
+  <head>
+    <title>CSGSS Department Info</title>
+    <?php $root = realpath($_SERVER["DOCUMENT_ROOT"]); include("$root/includes/head.html") ?>
+  </head>
+  <body>
+    <div class="container">
+      <?php $root = realpath($_SERVER["DOCUMENT_ROOT"]); include("$root/includes/header.html") ?>
+      <div class="page_content">
+        <div class="resources">
+          <h1>Department Info</h1>
+
+          <div><p class="heading">Reading groups</p></div>
+          <div class="content">
+            <ul>
+              <li><a href="http://www.math.rutgers.edu/~jmgilmer/semFall2013.html">Theory of Computing Reading Seminar</a><br/>Wednesdays at 9:30 AM in Core 433</li>
+
+              <li><a href="http://paul.rutgers.edu/~zy56/reading.group/index.html">Programming Language, Architecture, Compiler Reading Group</a><br/>Fridays at 3 PM in Core B</li>
+            </ul>
+          </div>
+
+          <div><p class="heading">Colloquiua and seminars</p></div>
+          <div class="content">
+            <ul>
+              <li><a href="http://dimacs.rutgers.edu/Seminars/interseminars13-14.html">DIMACS/CCICADA Interdisciplinary Seminar</a><br/>Mondays at 11 AM in Core 431</li>
+
+              <li>CS Department Colloquium Series<br/>Tuesdays at 11 AM in Core 101</li>
+
+              <li><a href="http://www.cs.rutgers.edu/~santosh.nagarakatte/RAPLSeminar.html">Rutgers Architecture and Programming Language Seminar Series</a><br/>(a subset of the CS Department Colloquium Series)</li>
+
+              <li><a href="http://www.math.rutgers.edu/seminars/allseminars.php?sem_name=Discrete%20Math">Discrete math seminar</a><br/>Tuesdays at 2 PM in Hill 525</li>
+
+             <li><a href="http://www.math.rutgers.edu/~sk1233/theory-seminar/F13/">Theory seminar</a><br/>Wednesdays at 11 AM in Core 431</li>
+
+              <li><a href="http://www.math.rutgers.edu/seminars/calendar.php">All math seminars</a></li>
+              <li><a href="http://www.cs.rutgers.edu/news/colloquia/">All CS seminars</a></li>
+            </ul>
+          </div>
+
+        </div>
+      </div>
+      <?php $root = realpath($_SERVER["DOCUMENT_ROOT"]); include("$root/includes/footer.html") ?>
+    </div>
+  </body>
+</html>
