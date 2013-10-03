f9158c3f77daf4477e172bc62238a6ceba0f735d
Jeff Ames <jeff@anjefu.com>
2013-10-03 12:17:44 -0400

    Add days of the week to news page dates

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
           <div class="newsitem">
             <div>
               <h2>Potluck Lunch</h2>
-              <h3>Sept. 26, 2013</h3>
+              <h3>Thu., Sept. 26, 2013</h3>
               <p>Bring a dish to share with your colleagues! We'll provide drinks. Please contact Brian McMahan to coordinate food items.</p>
               <p>12:00 PM in CoRE 301</p>
             </div>
@@ -36,7 +36,7 @@
           <div class="newsitem">
             <div>
               <h2>Fall CS Orientation</h2>
-              <h3>Sept. 4, 2013</h3>
+              <h3>Wed., Sept. 4, 2013</h3>
               <p>The fall CS orientation is an event to help introduce and acquaint new and existing graduate students and the faculty of the department. It highlights some of the research done in the department and allows an opportunity for new graduate students to solicit advice from more senior students. Please come out and meet your new colleagues!</p>
               <p>It will be held from 9:30 AM to 1:00 PM in CoRE 301</p>
               <table>
@@ -54,7 +54,7 @@
           <div class="newsitem">
             <div>
               <h2>End of Year Social</h2>
-              <h3>May 14, 2013</h3>
+              <h3>Tues., May 14, 2013</h3>
               <p>To celebrate the conclusion of the academic year, CSGSS and the CS department are holding an end-of-semester social. There will be food (from Efe's) and drinks provided, thanks to a generous contribution from Google.</p>
               <p>12:00 PM &ndash; 2:00 PM, in the CoRE lobby.</p>
             </div>
@@ -64,7 +64,7 @@
           <div class="newsitem">
             <div>
               <h2>April CSGSS Social</h2>
-              <h3>April 16, 2013</h3>
+              <h3>Tues., April 16, 2013</h3>
 			  <p>Join your fellow CS graduate students for lunch and socializing. A great opportunity
 			    to get out of your lab or office and see how the rest of the department is doing.  Don't
 			    forget to bring your appetite!</p>
@@ -76,7 +76,7 @@
           <div class="newsitem">
             <div>
               <h2>March CSGSS Social</h2>
-              <h3>March 26, 2013</h3>
+              <h3>Tues., March 26, 2013</h3>
 			  <p>Join your fellow CS graduate students for lunch and socializing. A great opportunity
 			    to get out of your lab or office and see how the rest of the department is doing.  Don't
 			    forget to bring your appetite!</p>
@@ -89,7 +89,7 @@
             <div class="thumbnail"><img src="/images/google-phd-mixer.png" height=299 width=400 alt="Google PhD mixer" class="thumbnail"/></div>
             <div>
               <h2>Google PhD Mixer</h2>
-              <h3>Feb 11, 2013</h3>
+              <h3>Mon., Feb 11, 2013</h3>
               <p>RU PhD Googlers will discuss their research interests and work at Google as well as mingle with current PhD students</p>
               <p>5:00 PM &ndash; 7:00 PM in CoRE A</p>
             </div>
@@ -100,7 +100,7 @@
             <div class="thumbnail"><img src="/images/fall2012.jpg" height=299 width=400 alt="Rutgers in fall" class="thumbnail"/></div>
             <div>
               <h2>Welcome back!</h2>
-              <h3>Sept 4, 2012</h3>
+              <h3>Tues., Sept 4, 2012</h3>
               <p>Welcome back for Fall 2012!</p>
             </div>
             <div class="clear"></div>
