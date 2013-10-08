7539fa2cd048000400a1af90e5ab60c607038bc1
Jeff Ames <jeff@anjefu.com>
2013-10-08 13:13:39 -0400

    Add October lunch social

--- index.php
+++ index.php
@@ -13,10 +13,22 @@
 
           <div class="newsitem">
             <div>
+              <h2>October CSGSS Social</h2>
+              <h3>Wed., Oct 9, 2013</h3>
+			  <p>Join your fellow CS graduate students for lunch and socializing. A great opportunity
+			    to get out of your lab or office and see how the rest of the department is doing.  Don't
+			    forget to bring your appetite!</p>
+			  <p>12:00 &ndash; 1:30 PM in Core A</p>
+            </div>
+            <div class="clear"></div>
+          </div>
+
+          <div class="newsitem">
+            <div>
               <h2>Potluck Lunch</h2>
               <h3>Thu., Sept. 26, 2013</h3>
               <p>Bring a dish to share with your colleagues! We'll provide drinks. Please contact Brian McMahan to coordinate food items.</p>
-              <p>12:00 PM in CoRE 301</p>
+              <p>12:00 PM in Core 301</p>
             </div>
             <div class="clear"></div>
           </div>
@@ -28,7 +40,7 @@
 			  <p>Join your fellow CS graduate students for lunch and socializing. A great opportunity
 			    to get out of your lab or office and see how the rest of the department is doing.  Don't
 			    forget to bring your appetite!</p>
-			  <p>12:00 &ndash; 1:30 PM in CoRE A</p>
+			  <p>12:00 &ndash; 1:30 PM in Core A</p>
             </div>
             <div class="clear"></div>
           </div>
@@ -38,7 +50,7 @@
               <h2>Fall CS Orientation</h2>
               <h3>Wed., Sept. 4, 2013</h3>
               <p>The fall CS orientation is an event to help introduce and acquaint new and existing graduate students and the faculty of the department. It highlights some of the research done in the department and allows an opportunity for new graduate students to solicit advice from more senior students. Please come out and meet your new colleagues!</p>
-              <p>It will be held from 9:30 AM to 1:00 PM in CoRE 301</p>
+              <p>It will be held from 9:30 AM to 1:00 PM in Core 301</p>
               <table>
                 <tr><td>9:30 &ndash; 9:45</td><td>coffee and light breakfast</td></tr>
                 <tr><td>9:45 &ndash; 10:30</td><td>welcome</td></tr>
@@ -56,7 +68,7 @@
               <h2>End of Year Social</h2>
               <h3>Tues., May 14, 2013</h3>
               <p>To celebrate the conclusion of the academic year, CSGSS and the CS department are holding an end-of-semester social. There will be food (from Efe's) and drinks provided, thanks to a generous contribution from Google.</p>
-              <p>12:00 PM &ndash; 2:00 PM, in the CoRE lobby.</p>
+              <p>12:00 PM &ndash; 2:00 PM, in the Core lobby.</p>
             </div>
             <div class="clear"></div>
           </div>
@@ -68,7 +80,7 @@
 			  <p>Join your fellow CS graduate students for lunch and socializing. A great opportunity
 			    to get out of your lab or office and see how the rest of the department is doing.  Don't
 			    forget to bring your appetite!</p>
-			  <p>12:00 &ndash; 1:25 PM in CoRE A</p>
+			  <p>12:00 &ndash; 1:25 PM in Core A</p>
             </div>
             <div class="clear"></div>
           </div>
@@ -80,7 +92,7 @@
 			  <p>Join your fellow CS graduate students for lunch and socializing. A great opportunity
 			    to get out of your lab or office and see how the rest of the department is doing.  Don't
 			    forget to bring your appetite!</p>
-			  <p>12:00 &ndash; 1:25 PM in CoRE A</p>
+			  <p>12:00 &ndash; 1:25 PM in Core A</p>
             </div>
             <div class="clear"></div>
           </div>
@@ -91,7 +103,7 @@
               <h2>Google PhD Mixer</h2>
               <h3>Mon., Feb 11, 2013</h3>
               <p>RU PhD Googlers will discuss their research interests and work at Google as well as mingle with current PhD students</p>
-              <p>5:00 PM &ndash; 7:00 PM in CoRE A</p>
+              <p>5:00 PM &ndash; 7:00 PM in Core A</p>
             </div>
             <div class="clear"></div>
           </div>
