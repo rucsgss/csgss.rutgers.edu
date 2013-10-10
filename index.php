572e39d7e881381ef30d3e436d600c596180e717
Jeff Ames <jeff@anjefu.com>
2013-10-10 00:23:00 -0400

    Add news archive. Add image for openhatch event

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
         <div class="news">
 
           <div class="newsitem">
+            <div class="thumbnail"><img src="/images/openhatch.png" height=300 width=400 alt="OpenHatch logo" class="thumbnail"/></div>
             <div>
               <h2>Open Source Comes to Rutgers</h2>
               <h3>Mon., Oct 21, 2013</h3>
@@ -75,62 +76,10 @@
             <div class="clear"></div>
           </div>
 
-          <div class="newsitem">
-            <div>
-              <h2>End of Year Social</h2>
-              <h3>Tues., May 14, 2013</h3>
-              <p>To celebrate the conclusion of the academic year, CSGSS and the CS department are holding an end-of-semester social. There will be food (from Efe's) and drinks provided, thanks to a generous contribution from Google.</p>
-              <p>12:00 PM &ndash; 2:00 PM, in the Core lobby.</p>
-            </div>
-            <div class="clear"></div>
-          </div>
-
-          <div class="newsitem">
-            <div>
-              <h2>April CSGSS Social</h2>
-              <h3>Tues., April 16, 2013</h3>
-			  <p>Join your fellow CS graduate students for lunch and socializing. A great opportunity
-			    to get out of your lab or office and see how the rest of the department is doing.  Don't
-			    forget to bring your appetite!</p>
-			  <p>12:00 &ndash; 1:25 PM in Core A</p>
-            </div>
-            <div class="clear"></div>
-          </div>
-
-          <div class="newsitem">
-            <div>
-              <h2>March CSGSS Social</h2>
-              <h3>Tues., March 26, 2013</h3>
-			  <p>Join your fellow CS graduate students for lunch and socializing. A great opportunity
-			    to get out of your lab or office and see how the rest of the department is doing.  Don't
-			    forget to bring your appetite!</p>
-			  <p>12:00 &ndash; 1:25 PM in Core A</p>
-            </div>
-            <div class="clear"></div>
-          </div>
-
-          <div class="newsitem">
-            <div class="thumbnail"><img src="/images/google-phd-mixer.png" height=299 width=400 alt="Google PhD mixer" class="thumbnail"/></div>
-            <div>
-              <h2>Google PhD Mixer</h2>
-              <h3>Mon., Feb 11, 2013</h3>
-              <p>RU PhD Googlers will discuss their research interests and work at Google as well as mingle with current PhD students</p>
-              <p>5:00 PM &ndash; 7:00 PM in Core A</p>
-            </div>
-            <div class="clear"></div>
-          </div>
-
-          <div class="newsitem">
-            <div class="thumbnail"><img src="/images/fall2012.jpg" height=299 width=400 alt="Rutgers in fall" class="thumbnail"/></div>
-            <div>
-              <h2>Welcome back!</h2>
-              <h3>Tues., Sept 4, 2012</h3>
-              <p>Welcome back for Fall 2012!</p>
-            </div>
-            <div class="clear"></div>
-          </div>
-
         </div>
+
+        <p>For older news items, check out the <a href="/news-archive">news archive</a>.</p>
+
       </div>
       <?php $root = realpath($_SERVER["DOCUMENT_ROOT"]); include("$root/includes/footer.html") ?>
     </div>
