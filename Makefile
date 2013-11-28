a7e33a8c20f279b5a042ec95e70254c74a165f8c
Jeff Ames <jeff@anjefu.com>
2013-11-28 11:56:20 -0500

    Change default.css to style.css, remove minimization for simplicity.

--- Makefile
+++
@@ -1,9 +0,0 @@
-OUTPUT = default-min.css
-
-all: $(OUTPUT)
-
-default-min.css: default.css
-	yuicompressor -o $@ $<
-
-clean:
-	@rm -f $(OUTPUT) default-min.css
--- deploy.sh
+++ deploy.sh
@@ -6,11 +6,6 @@ USERNAME="jca105"
 SERVER="pacman.cs.rutgers.edu"
 SERVERPATH="~/csgss"
 
-if [ ! -f "./default-min.css" ]; then
-    echo "Error: default-min.css does not exist. Please run \"make\" first." 1>&2
-    exit 1
-fi
-
 rsync -avz -e ssh --exclude-from file-excludes.txt --delete --delete-excluded . ${USERNAME}@${SERVER}:${SERVERPATH}
 
 echo ""
