ef168a441b1b975b2e3f0c6787823389cb870351
Jeff Ames <jeff@anjefu.com>
2014-02-26 15:42:36 -0500

    Update department info HTML formatting, R-PARC meeting time

--- department-info/index.php
+++ department-info/index.php
@@ -14,26 +14,35 @@
           <div><p class="heading">Reading groups</p></div>
           <div class="content">
             <ul>
-              <li><a href="http://paul.rutgers.edu/~zy56/reading.group/index.html">Programming Languages, Architecture, and Compilers Reading Group</a><br/>Tuesdays at 3 PM in Core B</li>
+              <li><a href="http://paul.rutgers.edu/~zy56/reading.group/index.html">Programming Languages, Architecture, and Compilers Reading Group</a><br/>
+                Tuesdays at 10 AM in Core B</li>
 
-              <li><a href="https://www.math.rutgers.edu/~tnaumovi/seminar.html">Theory of Computing Reading Seminar</a><br/>Wednesdays at 9:30 AM in Core 433</li>
+              <li><a href="https://www.math.rutgers.edu/~tnaumovi/seminar.html">Theory of Computing Reading Seminar</a><br/>
+                Wednesdays at 9:30 AM in Core 433</li>
 
-              <li>CS Theory-Grad Reading Group<br />(email <a href="mailto:cstheory-grad-on@dragon.rutgers.edu">cstheory-grad-on@dragon.rutgers.edu</a> to subscribe to the mailing list)<br/>Wednesdays at 2 PM in Core A</li>
+              <li>CS Theory-Grad Reading Group<br />
+                (email <a href="mailto:cstheory-grad-on@dragon.rutgers.edu">cstheory-grad-on@dragon.rutgers.edu</a> to subscribe to the mailing list)<br/>
+                Wednesdays at 2 PM in Core A</li>
             </ul>
           </div>
 
           <div><p class="heading">Colloquiua and seminars</p></div>
           <div class="content">
             <ul>
-              <li><a href="http://dimacs.rutgers.edu/Seminars/interseminars13-14.html">DIMACS/CCICADA Interdisciplinary Seminar</a><br/>Tuesdays (sometimes Mondays) at 11 AM in Core 431</li>
+              <li><a href="http://dimacs.rutgers.edu/Seminars/interseminars13-14.html">DIMACS/CCICADA Interdisciplinary Seminar</a><br/>
+                Tuesdays (sometimes Mondays) at 11 AM in Core 431</li>
 
-              <li><a href="http://www.cs.rutgers.edu/~jca105/cs-colloquium/">CS Department Colloquium Series</a><br/>Tuesdays at 11 AM in Core 101</li>
+              <li><a href="http://www.cs.rutgers.edu/~jca105/cs-colloquium/">CS Department Colloquium Series</a><br/>
+                Tuesdays at 11 AM in Core 101</li>
 
-              <li><a href="http://www.math.rutgers.edu/seminars/allseminars.php?sem_name=Discrete%20Math">Discrete Math Seminar</a><br/>Tuesdays at 2 PM in Hill 525</li>
+              <li><a href="http://www.math.rutgers.edu/seminars/allseminars.php?sem_name=Discrete%20Math">Discrete Math Seminar</a><br/>
+                Tuesdays at 2 PM in Hill 525</li>
 
-              <li><a href="http://www.math.rutgers.edu/~sk1233/theory-seminar/S14/">Rutgers/DIMACS Theory of Computing Seminar</a><br/>Wednesdays at 11 AM in Core 301</li>
+              <li><a href="http://www.math.rutgers.edu/~sk1233/theory-seminar/S14/">Rutgers/DIMACS Theory of Computing Seminar</a><br/>
+                Wednesdays at 11 AM in Core 301</li>
 
-              <li><a href="http://www.stat.rutgers.edu/home/pingli/bigdata.html">Statistical Learning and Computation for Big Data Analysis</a><br/>Thursdays at noon in Hill 552 (Lunch at 11:40 AM in Hill 502)</li>
+              <li><a href="http://www.stat.rutgers.edu/home/pingli/bigdata.html">Statistical Learning and Computation for Big Data Analysis</a><br/>
+                Thursdays at noon in Hill 552 (Lunch at 11:40 AM in Hill 502)</li>
 
               <li><a href="http://www.math.rutgers.edu/seminars/calendar.php">All math seminars</a></li>
               <li><a href="http://www.cs.rutgers.edu/news/colloquia/">All CS seminars</a></li>
