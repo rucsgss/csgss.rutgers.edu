607553efc7159916b54067ee347edc0f10d7adf4
Jeff Ames <jeff@anjefu.com>
2014-06-26 15:18:58 -0400

    Remove unused files

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-USERNAME="jeff"
-SERVER="192.168.56.101"
-SERVERPATH="/srv/http"
-
-rsync -avz -e ssh --exclude-from file-excludes.txt --delete --delete-excluded . ${USERNAME}@${SERVER}:${SERVERPATH}
