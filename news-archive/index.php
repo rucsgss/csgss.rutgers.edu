4fca3d49086cbaba08e588f06a38414f347b3529
Jeff Ames <jeff@anjefu.com>
2014-08-31 10:23:03 -0400

    Move older news items to news-archive

--- news-archive/index.php
+++ news-archive/index.php
@@ -8,6 +8,163 @@
     <div class="container">
       <?php $root = realpath($_SERVER["DOCUMENT_ROOT"]); include("$root/includes/header.html") ?>
       <div class="page_content">
+        <h1>2013 &ndash; 2014</h1>
+        <div class="news">
+
+          <div class="newsitem">
+            <div>
+              <h2>End of Year Social</h2>
+              <h3>Wed., May 7, 2014</h3>
+              <p>To celebrate the conclusion of the academic year, CSGSS and the CS department are holding an end-of-semester lunch social. There will be catered food and drinks provided.</p>
+              <p>12:30 PM &ndash; 2:00 PM, on the lawn outside Core.</p>
+            </div>
+            <div class="clear"></div>
+          </div>
+
+          <div class="newsitem">
+            <div>
+              <h2>March CSGSS Social</h2>
+              <h3>Wed., Mar 26, 2014</h3>
+			  <p>Join your fellow CS graduate students for lunch and socializing. A great opportunity
+			    to get out of your lab or office and see how the rest of the department is doing.  Don't
+			    forget to bring your appetite!</p>
+			  <p>12:00 &ndash; 1:30 PM in Core 301</p>
+            </div>
+            <div class="clear"></div>
+          </div>
+
+          <div class="newsitem">
+            <div>
+              <h2>February CSGSS Social</h2>
+              <h3>Fri., Feb. 28, 2014</h3>
+			  <p>Join your fellow CS graduate students for lunch and socializing. A great opportunity
+			    to get out of your lab or office and see how the rest of the department is doing.  Don't
+			    forget to bring your appetite!</p>
+			  <p>12:00 &ndash; 1:30 PM in Core 301</p>
+            </div>
+            <div class="clear"></div>
+          </div>
+
+          <div class="newsitem">
+            <div>
+              <h2>December CSGSS Social</h2>
+              <h3>Wed., Dec. 4, 2013</h3>
+			  <p>Join your fellow CS graduate students for lunch and socializing. A great opportunity
+			    to get out of your lab or office and see how the rest of the department is doing.  Don't
+			    forget to bring your appetite!</p>
+			  <p>12:00 &ndash; 1:30 PM in Core 301</p>
+            </div>
+            <div class="clear"></div>
+          </div>
+
+          <div class="newsitem">
+            <div>
+              <h2>November CSGSS Social</h2>
+              <h3>Wed., Nov. 6, 2013</h3>
+			  <p>Join your fellow CS graduate students for lunch and socializing. A great opportunity
+			    to get out of your lab or office and see how the rest of the department is doing.  Don't
+			    forget to bring your appetite!</p>
+			  <p>12:00 &ndash; 1:30 PM in Core 301</p>
+            </div>
+            <div class="clear"></div>
+          </div>
+
+          <div class="newsitem">
+            <div>
+              <h2>Faculty talk: Shubhangi Saraf</h2>
+              <h3>Fri., Nov. 1, 2013</h3>
+              <p>Shubhangi's research interests lie in theoretical computer science and discrete mathematics. Her research draws on tools from mathematics such as probability theory, algebra and discrete geometry to better understand some of the basic questions in theoretical computer science in areas such as the complexity of arithmetic computation, randomness in computing, property testing and coding theory.</p>
+			  <p>1:00 &ndash; 2:00 PM in Core 301</p>
+            </div>
+            <div class="clear"></div>
+          </div>
+
+          <div class="newsitem">
+            <div>
+              <h2>Potluck Lunch</h2>
+              <h3>Wed., Oct. 23, 2013</h3>
+              <p>Bring a dish to share with your colleagues! We'll provide drinks. Please contact Brian McMahan to coordinate food items.</p>
+              <p>12:00 PM in Core 301</p>
+            </div>
+            <div class="clear"></div>
+          </div>
+
+          <div class="newsitem">
+            <div class="thumbnail"><img src="/images/openhatch.png" height=300 width=400 alt="OpenHatch logo" class="thumbnail"/></div>
+            <div>
+              <h2>Open Source Comes to Rutgers</h2>
+              <h3>Mon., Oct. 21, 2013</h3>
+              <p>Shauna Gordon-McKeon from <a href="http://openhatch.org/">OpenHatch.org</a> will be helping students get acquainted with and learn about open source:</p>
+              <p>"We'll teach you about open source licensing, collaboration tools, and how free software projects are organized. Then we'll help you make contributions to open source projects. And throughout, we'll feed you, get to know you, and talk with you about opportunities for students in open source."</p>
+			  <p><a href="http://rutgers.openhatch.org/">http://rutgers.openhatch.org/</a></p>
+			  <p>11:30 &ndash; 5:30 PM in Core 301</p>
+            </div>
+            <div class="clear"></div>
+          </div>
+
+          <div class="newsitem">
+            <div>
+              <h2>Faculty talk: Santosh Nagarakatte</h2>
+              <h3>Fri., Oct. 18, 2013</h3>
+              <p>Santosh's research interests lie in the area of hardware-software interfaces spanning programming languages, compilers, and computer architecture to build robust and secure software systems. This line of work explores new programming abstractions, principles and runtime environments that enable programmers to productively write correct and bug-free software.</p>
+			  <p>1:00 &ndash; 2:00 PM in Core 301</p>
+            </div>
+            <div class="clear"></div>
+          </div>
+
+          <div class="newsitem">
+            <div>
+              <h2>October CSGSS Social</h2>
+              <h3>Wed., Oct. 9, 2013</h3>
+			  <p>Join your fellow CS graduate students for lunch and socializing. A great opportunity
+			    to get out of your lab or office and see how the rest of the department is doing.  Don't
+			    forget to bring your appetite!</p>
+			  <p>12:00 &ndash; 1:30 PM in Core 301</p>
+            </div>
+            <div class="clear"></div>
+          </div>
+
+          <div class="newsitem">
+            <div>
+              <h2>Potluck Lunch</h2>
+              <h3>Thu., Sept. 26, 2013</h3>
+              <p>Bring a dish to share with your colleagues! We'll provide drinks. Please contact Brian McMahan to coordinate food items.</p>
+              <p>12:00 PM in Core 301</p>
+            </div>
+            <div class="clear"></div>
+          </div>
+
+          <div class="newsitem">
+            <div>
+              <h2>September CSGSS Social</h2>
+              <h3>Wed., Sept. 11, 2013</h3>
+			  <p>Join your fellow CS graduate students for lunch and socializing. A great opportunity
+			    to get out of your lab or office and see how the rest of the department is doing.  Don't
+			    forget to bring your appetite!</p>
+			  <p>12:00 &ndash; 1:30 PM in Core 301</p>
+            </div>
+            <div class="clear"></div>
+          </div>
+
+          <div class="newsitem">
+            <div>
+              <h2>Fall CS Orientation</h2>
+              <h3>Wed., Sept. 4, 2013</h3>
+              <p>The fall CS orientation is an event to help introduce and acquaint new and existing graduate students and the faculty of the department. It highlights some of the research done in the department and allows an opportunity for new graduate students to solicit advice from more senior students. Please come out and meet your new colleagues!</p>
+              <p>It will be held from 9:30 AM to 1:00 PM in Core 301</p>
+              <table>
+                <tr><td>9:30 &ndash; 9:45</td><td>coffee and light breakfast</td></tr>
+                <tr><td>9:45 &ndash; 10:30</td><td>welcome</td></tr>
+                <tr><td>10:30 &ndash; 11:00</td><td>faculty talk (Prof. Matthew Stone)</td></tr>
+                <tr><td>11:00 &ndash; 11:30</td><td>faculty talk (Prof. Martin Farach-Colton, Graduate Program Director)</td></tr>
+                <tr><td>11:30 &ndash; 12:00</td><td>faculty talk (Prof. Ricardo Bianchini)</td></tr>
+                <tr><td>12:00 &ndash; 1:00</td><td>lunch and grad student panel</td></tr>
+              </table>
+            </div>
+            <div class="clear"></div>
+          </div>
+
+        </div>
         <h1>2012 &ndash; 2013</h1>
         <div class="news">
 
