f1f3fb75151f3abd86c386004bd04a8e55562ab7
Jeff Ames <jeff@anjefu.com>
2015-09-16 09:30:41 -0400

    Add fall 2015 calendar, move older news to archive

--- news-archive/index.php
+++ news-archive/index.php
@@ -8,8 +8,198 @@
     <div class="container">
       <?php $root = realpath($_SERVER["DOCUMENT_ROOT"]); include("$root/includes/header.html") ?>
       <div class="page_content">
-        <h1>2013 &ndash; 2014</h1>
+
+        <h1>News archive</h1>
+        <ul>
+          <li><a href="#2014">2014 &ndash; 2015</a></li>
+          <li><a href="#2013">2013 &ndash; 2014</a></li>
+          <li><a href="#2012">2012 &ndash; 2013</a></li>
+        </ul>
+
+        <a name="2014"><h1>2014 &ndash; 2015</h1></a>
         <div class="news">
+
+          <div class="newsitem">
+            <div>
+              <h2>CSGSS Excellence in Teaching Award for 2015</h2>
+              <h3>Wed., May 6, 2015</h3>
+              <p>We are happy to announce that this year's Excellence in Teaching Award has been awarded to <a href="http://www.pracsyslab.org/bekris/">Professor Kostas Bekris</a>. Congratulations!</p>
+            </div>
+            <div class="clear"></div>
+          </div>
+
+          <div class="newsitem">
+            <div>
+              <h2>Spring 2015 Calendar</h2>
+              <h3>Sun., Feb 1, 2015</h3>
+              <p>Lunch socials: Noon in Core 301 on 2/2, 2/23, and 4/6</p>
+              <p>Game nights: 5 pm in Core 301 on 2/7 and 4/4</p>
+              <p>Coffee hours: 2 pm Fridays in the Core 3rd floor lounge</p>
+            </div>
+            <div class="clear"></div>
+          </div>
+
+          <div class="newsitem">
+            <div class="thumbnail"><img src="/images/openhatch.png" height=300 width=400 alt="OpenHatch logo" class="thumbnail"/></div>
+            <div>
+              <h2>Open Source Comes to Rutgers</h2>
+              <h3>Sat., Jan 31, 2015</h3>
+              <p><a href="http://openhatch.org/">OpenHatch.org</a> will be helping students get acquainted with and learn about open source:</p>
+              <p>"We'll teach you about open source licensing, collaboration tools, and how free software projects are organized. Then we'll help you make contributions to open source projects. And throughout, we'll feed you, get to know you, and talk with you about opportunities for students in open source."</p>
+			  <p><a href="http://rutgers.openhatch.org/">http://rutgers.openhatch.org/</a></p>
+			  <p>12:00 &ndash; 5:30 PM in the Cove in the BCC</p>
+            </div>
+            <div class="clear"></div>
+          </div>
+
+          <div class="newsitem">
+            <div class="thumbnail"><img src="/images/google-tech-talk.png" height=299 width=400 alt="Google PhD tech talk" class="thumbnail"/></div>
+            <div>
+              <h2>Google PhD Info Session</h2>
+              <h3>Thu., Dec 11, 2014</h3>
+              <p>There are many opportunities for PhDs at Google to
+              make immediate impact and work on real problems.  Google
+              strives to provide an environment of on-going learning
+              where advanced technical development and innovation are
+              the priorities.  Our engineers are also encouraged to
+              make contributions to the computer science community
+              through a variety of professional development
+              activities, such as attending conferences, publishing
+              papers, open source, and more.</p>
+
+              <p>To learn more, we invite you to join us at our PhD
+              Info Session this Thursday, December 11th @ 5:30pm!
+              Come grab a bite, learn more about our PhD-specific
+              opportunities and hear from Rutgers PhD alumni about
+              their work!</p>
+
+              <p>Go <a href="https://docs.google.com/forms/d/152GZmfLi4FPpeDFwxJzAYp3oXv77fHcRNj-ohEhym1I/viewform">here</a> to RSVP for the event!</p>
+
+              <p>5:30 PM &ndash; 7:30 PM in Core 301</p>
+            </div>
+            <div class="clear"></div>
+          </div>
+
+          <div class="newsitem">
+            <div>
+              <h2>December Lunch Social</h2>
+              <h3>Thurs., Dec. 11, 2014</h3>
+			  <p>Join your fellow CS graduate students for lunch and socializing. A great opportunity
+			    to get out of your lab or office and see how the rest of the department is doing.  Don't
+			    forget to bring your appetite!</p>
+			  <p>12:30 &ndash; 2:00 PM in Core 301</p>
+            </div>
+            <div class="clear"></div>
+          </div>
+
+          <div class="newsitem">
+            <div>
+              <h2>First-years Checkpoint Meeting</h2>
+              <h3>Fri., Dec. 5, 2014</h3>
+			  <p>Figuring out grad school and all the details of your program can be challenging.
+                This meeting is a chance to meet with senior grad students, as well as the Graduate
+                Director and MS Program Coordinator, to get answers to any questions you might have
+                about classes, research, teaching, program requirements, or grad student life in
+                general.</p>
+              <p>First-year students are encouraged to attend, but all years are welcome to come
+                and share your experience!</p>
+			  <p>12:30 &ndash; 2:00 PM in Core 301</p>
+            </div>
+            <div class="clear"></div>
+          </div>
+
+          <div class="newsitem">
+            <div>
+              <h2>November Lunch Social</h2>
+              <h3>Thurs., Nov. 13, 2014</h3>
+			  <p>Join your fellow CS graduate students for lunch and socializing. A great opportunity
+			    to get out of your lab or office and see how the rest of the department is doing.  Don't
+			    forget to bring your appetite!</p>
+			  <p>12:30 &ndash; 2:00 PM in Core 301</p>
+            </div>
+            <div class="clear"></div>
+          </div>
+
+          <div class="newsitem">
+            <div>
+              <h2>October Lunch Social</h2>
+              <h3>Thurs., Oct. 23, 2014</h3>
+			  <p>Join your fellow CS graduate students for lunch and socializing. A great opportunity
+			    to get out of your lab or office and see how the rest of the department is doing.  Don't
+			    forget to bring your appetite!</p>
+			  <p>12:00 &ndash; 1:30 PM in Core 301</p>
+            </div>
+            <div class="clear"></div>
+          </div>
+
+          <div class="newsitem">
+            <div>
+              <h2>Coffee hours commence!</h2>
+              <h3>Mon., Sept. 29, 2014</h3>
+			  <p>We're excited to announce coffee hours at 3 PM on Mondays and Wednesdays this semester! Come by, hang out, and recharge!</p>
+			  <p>Coffee hours on Mondays will be held in CBIM.</p>
+			  <p>Coffee hours on Wednesdays will be held in the 3rd floor lobby of Core.</p>
+			  <p>3:00 &ndash; 4:00 PM</p>
+            </div>
+            <div class="clear"></div>
+          </div>
+
+          <div class="newsitem">
+            <div>
+              <h2>September Lunch Social / Orientation</h2>
+              <h3>Thurs., Sept. 18, 2014</h3>
+			  <p>Welcome all new CS graduate students! Come meet the new and old grad students at our first lunch social / CSGSS orientation! We'll introduce the new CSGSS officers and announce a few of the things we have planned for the coming year.</p>
+			  <p>12:00 &ndash; 1:30 PM in Core 301</p>
+            </div>
+            <div class="clear"></div>
+          </div>
+
+          <div class="newsitem">
+            <div>
+              <h2>Welcome!</h2>
+              <h3>Fri., Aug 29, 2014</h3>
+              <p>Welcome to all the new graduate students this year!</p>
+            </div>
+            <div class="clear"></div>
+          </div>
+
+        </div>
+        <a name="2013"><h1>2013 &ndash; 2014</h1></a>
+        <div class="news">
+
+          <div class="newsitem">
+            <div>
+              <h2>Farewell till the fall!</h2>
+              <h3>Fri., May 16, 2014</h3>
+              <a href="/images/department-photo-2014.png"><img src="/images/department-photo-2014.jpg" height=215 width=800 alt="Group photo at the end-of-year social" class="thumbnail"/></a>
+              <p>Congratulations to everyone graduating this year, and have a great summer!</p>
+            </div>
+            <div class="clear"></div>
+          </div>
+
+          <div class="newsitem">
+            <div>
+              <h2>CSGSS Officers for 2014 &ndash; 2015</h2>
+              <h3>Thu., May 15, 2014</h3>
+              <p>Election results are in!</p>
+              <ul>
+                <li>President: Brian McMahan</li>
+                <li>Vice President: Jeff Ames</li>
+                <li>Treasurer: Pracheeti Maheshwari</li>
+              </ul>
+            </div>
+            <div class="clear"></div>
+          </div>
+
+          <div class="newsitem">
+            <div>
+              <h2>CSGSS Excellence in Teaching Award for 2014</h2>
+              <h3>Wed., May 7, 2014</h3>
+              <div class="thumbnail"><img src="/images/shubhangi-teaching-award-2014.jpg" height=250 width=200 alt="Prof. Shubhangi accepting award" class="thumbnail"/></div>
+              <p>We are happy to announce that this year's Excellence in Teaching Award has been awarded to <a href="http://math.rutgers.edu/~ss1984/">Professor Shubhangi Saraf</a>. Congratulations!</p>
+            </div>
+            <div class="clear"></div>
+          </div>
 
           <div class="newsitem">
             <div>
@@ -165,7 +355,7 @@
           </div>
 
         </div>
-        <h1>2012 &ndash; 2013</h1>
+        <a name="2012"><h1>2012 &ndash; 2013</h1></a>
         <div class="news">
 
           <div class="newsitem">
