dbf9770a7f1df34530c8b6682b6da54ef8b4cb3c
Jeff Ames <jeff@anjefu.com>
2015-10-21 14:38:14 -0400

    Update F15 event calendar

--- index.php
+++ index.php
@@ -16,8 +16,8 @@
               <h2>Fall 2015 Calendar</h2>
               <h3>Tue., Sept. 8, 2015</h3>
               <h4>Lunch socials</h4>
-              <p>11 am - 12 pm in Core 301</p>
-              <p>9/24, 10/29, and 12/3</p>
+              <p>1 - 3 pm in Core 301</p>
+              <p>10/19 and 12/3</p>
               <h4>Coffee hours</h4>
               <p>3 - 4 pm in Core 301</p>
               <p>9/10, 9/25, 10/2, 10/9, 10/16, 10/23, 10/30, 11/6, 11/20, and 12/4</p>
